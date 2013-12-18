95b25a6810c1cb800259cc2a3a34313583f57667
zzStone <jizhouli1990@gmail.com>
2013-12-18 00:53:41 -0500

    bitch

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+sys.environ.setdefault('/home/ubuntu/website/OST/mysite/')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
