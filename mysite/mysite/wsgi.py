96cbcbbbb272392b1dd646e29ab7e1b01e7e619f
zzStone <jizhouli1990@gmail.com>
2013-12-18 00:56:34 -0500

    libichi

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 import sys
+sys.path.append('/home/ubuntu/website/OST/mysite/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-sys.environ.setdefault('/home/ubuntu/website/OST/mysite/')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
